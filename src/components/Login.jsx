a268a6ea8b2991cd593ef962fcdb93ea3ceeb0af
salimtaiyo <ichigeki.ccCsc@hotmail.com>
2018-05-30 23:25:47 -0400

    added routes

---
+++ src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        <div> 
+            <h1> Login </h1>
+        </div>
+          <button>
+                Login with Gmail
+            </button>
+       
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -9,12 +9,28 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 
+import Login from './components/Login';
+import Header from './components/Header';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
+
+
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+    <BrowserRouter>
+    <div>
+    <Header />
+    <Switch>
+   <Route path="/" component={App } exact={true}/>
+   <Route path="/login" component={Login } exact={true}/>
+    </Switch>
+    </div>
+    
+    </BrowserRouter>
+        
     </Provider>, 
     document.getElementById('root'));
 registerServiceWorker();
